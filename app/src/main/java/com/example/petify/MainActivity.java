019790999dd8f538a2dd7a821536a1b27b890b2d
FedericoAmura <federicoamura@gmail.com>
2019-03-29 17:25:05 -0300

    Vinculacion con firebase para despues usar de manejo de usuarios. Main activity ahora hace un login/logout en facebook. Queda como TODO el pedir los datos del usuario vinculado y mostrarlos en los campos correspondientes

--- app/src/main/java/com/example/petify/MainActivity.java
+++ app/src/main/java/com/example/petify/MainActivity.java
@@ -1,13 +1,122 @@
 package com.example.petify;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    public CallbackManager callbackManager;
+
+    private LoginButton loginButton;
+    private TextView displayName, emailId;
+    private ImageView displayImage;
+
+    private int INTERNET_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        // Check for Internet permissions
+        // TODO este permiso ya viene por default en los ultimos androids. Sin embargo, pareceria ser la solucion que todos dicen para
+        // que el metodo useLoginInformation de mas abajo no termine tirando error, si tan solo entrara en el if...
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.INTERNET}, INTERNET_PERMISSION);
+        }
+
+        displayName = findViewById(R.id.display_name);
+        displayImage = findViewById(R.id.image_view);
+        emailId = findViewById(R.id.email);
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("email", "public_profile"));
+
+        // Creating CallbackManager
+        callbackManager = CallbackManager.Factory.create();
+
+        // Registering CallbackManager with the LoginButton
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.i("RegisterCallback", "onSuccess");
+                // Retrieving access token using the LoginResult
+                AccessToken accessToken = loginResult.getAccessToken();
+                useLoginInformation(accessToken);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.i("RegisterCallback", "onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.i("RegisterCallback", "onError");
+            }
+        });
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        if (isLoggedIn) useLoginInformation(accessToken);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void useLoginInformation(AccessToken accessToken) {
+        // TODO esto tira el siguiente error de hostname
+        // java.net.UnknownHostException: Unable to resolve host "graph.facebook.com": No address associated with hostname
+//        GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+//            @Override
+//            public void onCompleted(JSONObject object, GraphResponse response) {
+//                try {
+//                    String name = object.getString("name");
+//                    String email = object.getString("email");
+//                    String image = object.getJSONObject("picture").getJSONObject("data").getString("url");
+//                    displayName.setText(name);
+//                    emailId.setText(email);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//
+//        // Parameter setting with Bundle
+//        Bundle parameters = new Bundle();
+//        parameters.putString("fields", "id,name,picture.width(200)");
+//        request.setParameters(parameters);
+//        request.executeAsync();
     }
 }
