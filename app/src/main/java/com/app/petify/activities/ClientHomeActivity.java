5aaa4cd8295f2381bc771984674bb6be97737dc5
FedericoAmura <federicoamura@gmail.com>
2019-04-01 12:45:11 -0300

    Se agrega boton de logout en Home Activity para cerrar la sesion y volver al login

--- app/src/main/java/com/app/petify/activities/ClientHomeActivity.java
+++ app/src/main/java/com/app/petify/activities/ClientHomeActivity.java
@@ -2,24 +2,36 @@ package com.app.petify.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.app.petify.models.Client;
 import com.app.petify.utils.LocalStorage;
+import com.facebook.login.LoginManager;
 
 public class ClientHomeActivity extends AppCompatActivity {
 
     private TextView displayName;
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_client);
 
-        displayName = findViewById(R.id.display_name);
-
         Client client = LocalStorage.getClient();
 
+        displayName = findViewById(R.id.display_name);
         displayName.setText(client.name);
+
+        logoutButton = findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logOut();
+                finish();
+            }
+        });
     }
 }
