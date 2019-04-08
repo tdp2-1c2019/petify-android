214c8a4a4b1d7968c1b087c229381b81b7a56efe
FAmura <federicoamura@gmail.com>
2019-04-08 15:25:49 -0300

    Driver actualiza su ubicacion en tiempo real en la db de firebase

--- app/src/main/java/com/app/petify/activities/DriverHomeActivity.java
+++ app/src/main/java/com/app/petify/activities/DriverHomeActivity.java
@@ -1,7 +1,12 @@
 package com.app.petify.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +15,25 @@ import com.app.petify.R;
 import com.app.petify.models.Driver;
 import com.app.petify.utils.LocalStorage;
 import com.facebook.login.LoginManager;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DriverHomeActivity extends AppCompatActivity {
 
     private Button logoutButton;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private double wayLatitude = 0.0, wayLongitude = 0.0;
+    private int LOCATION_PERMISSION = 2;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +46,44 @@ public class DriverHomeActivity extends AppCompatActivity {
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                fusedLocationClient.removeLocationUpdates(locationCallback);
                 LoginManager.getInstance().logOut();
                 Intent navigationIntent = new Intent(DriverHomeActivity.this, MainActivity.class);
                 startActivity(navigationIntent);
             }
         });
+        logoutButton.setVisibility(View.INVISIBLE);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // Check for Location permissions
+        if (ContextCompat.checkSelfPermission(DriverHomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(DriverHomeActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION);
+        }
+
+        // Obtenemos la location del chofer y la actualizamos en Firebase
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(1000);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        wayLatitude = location.getLatitude();
+                        wayLongitude = location.getLongitude();
+                        mDatabase.child("ubicacion").child("lat").setValue(wayLatitude);
+                        mDatabase.child("ubicacion").child("lng").setValue(wayLongitude);
+                    }
+                }
+            }
+        };
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
     }
 }
