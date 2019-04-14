4827d717069e9c4709ff99cee46ad75e2ec9801e
agu <agufiuba@gmail.com>
2019-04-14 15:58:58 -0300

    color titulo

--- app/src/main/java/com/app/petify/activities/CargarViajeActivity.java
+++ app/src/main/java/com/app/petify/activities/CargarViajeActivity.java
@@ -1,15 +1,28 @@
 package com.app.petify.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.app.petify.R;
 
 public class CargarViajeActivity extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cargar_viaje);
+        button = findViewById(R.id.solicitar_viaje);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), SeleccionChoferActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
