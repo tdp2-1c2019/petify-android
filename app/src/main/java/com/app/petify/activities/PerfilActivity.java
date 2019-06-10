5929c26926bb144b85cd0a7fa0cd445b4f8a7db1
agu <agufiuba@gmail.com>
2019-06-10 14:50:31 -0300

    Implementa pantalla perfil

--- app/src/main/java/com/app/petify/activities/PerfilActivity.java
+++ app/src/main/java/com/app/petify/activities/PerfilActivity.java
@@ -1,28 +1,42 @@
 package com.app.petify.activities;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.app.petify.R;
+import com.app.petify.models.Usuario;
 import com.facebook.Profile;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PerfilActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private NavigationView navView;
     private TextView drawerTitle;
     private MenuItem itemPerfil;
+    private TextView nombre;
+    private TextView correo;
+    private TextView direccion;
+    private TextView tel;
+    private TextView rol;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         navView = findViewById(R.id.driver_nav_view);
         navView.setNavigationItemSelectedListener(this);
@@ -32,6 +46,49 @@ public class PerfilActivity extends AppCompatActivity implements NavigationView.
         SpannableString s = new SpannableString(itemPerfil.getTitle());
         s.setSpan(new ForegroundColorSpan(ContextCompat.getColor(getBaseContext(), R.color.com_facebook_blue)), 0, s.length(), 0);
         itemPerfil.setTitle(s);
+
+        nombre = findViewById(R.id.profile_name);
+        correo = findViewById(R.id.profile_mail);
+        direccion = findViewById(R.id.profile_address);
+        tel = findViewById(R.id.profile_tel);
+        rol = findViewById(R.id.profile_rol);
+
+        mDatabase.child("customers").child(Profile.getCurrentProfile().getId()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Usuario u = dataSnapshot.getValue(Usuario.class);
+                if (u != null) {
+                    nombre.setText(u.name);
+                    correo.setText(u.email);
+                    direccion.setText(u.direccion);
+                    tel.setText(u.telefono);
+                    rol.setText("Pasajero");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        mDatabase.child("drivers").child(Profile.getCurrentProfile().getId()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Usuario u = dataSnapshot.getValue(Usuario.class);
+                if (u != null) {
+                    nombre.setText(u.name);
+                    correo.setText(u.email);
+                    direccion.setText(u.direccion);
+                    tel.setText(u.telefono);
+                    rol.setText("Conductor");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
